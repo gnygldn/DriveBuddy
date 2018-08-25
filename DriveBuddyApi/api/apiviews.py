44ac204401df66461d34641b09236cfe322717d5
Güney Gülden <e1942101@ceng.metu.edu.tr>
2018-08-26 00:28:41 +0300

    new url path added to filter trips. Now trips are filtered when two dates are given to the url 'api/trips/1999010120050202/'. date format is yyyy/mm/dd and 2 dates should be given in order as start and end date.

--- DriveBuddyApi/api/apiviews.py
+++ DriveBuddyApi/api/apiviews.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from .models import Trip, Driver
 from .serializers import DriverSerializer, TripSerializer
+import datetime
+import sys
 
 class list_of_trips(generics.ListCreateAPIView):
 	queryset = Trip.objects.all()
@@ -16,4 +18,29 @@ class list_of_drivers(generics.ListCreateAPIView):
 class driver_details(generics.RetrieveDestroyAPIView):
 	queryset = Driver.objects.all()
 	serializer_class = DriverSerializer
+
+class list_of_trips_selected_between_dates(APIView):
+	print("Guney",file=sys.stderr)
+	def get(self, request, date1):
+		date = str(date1)
+		startyear = int(date[:4])
+		startmonth = int(date[4:6])
+		startday = int(date[6:8])
+		endyear = int(date[8:12])
+		endmonth = int(date[12:14])
+		endday = int(date[14:])
+		start = datetime.date(startyear,startmonth,startday)
+		end = datetime.date(endyear,endmonth,endday)
+
+		print (start,file=sys.stderr)
+		print (end,file=sys.stderr)
+		trips = Trip.objects.all()
+		filteredtrips = []
+		for trip in trips:
+			tripend = trip.start_date + datetime.timedelta(days = trip.duration)
+			if trip.start_date.date() <= start and tripend.date() >= end:
+				print(trip)
+				filteredtrips.append(trip)
+		data = TripSerializer(filteredtrips, many=True).data
+		return Response(data)
 
--- DriveBuddyApi/api/urls.py
+++ DriveBuddyApi/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from api import views
-from .apiviews import list_of_trips, list_of_drivers, driver_details
+import datetime
+from .apiviews import list_of_trips, list_of_drivers, driver_details, list_of_trips_selected_between_dates
 urlpatterns = [
 	path("api/drivers/", list_of_drivers.as_view(), name="list_of_drivers"),
 	path("api/drivers/<int:pk>/", driver_details.as_view(), name ="driver_details"),
 	path("api/trips/", list_of_trips.as_view(), name="list_of_trips"),
+	path("api/trips/<int:date1>/", list_of_trips_selected_between_dates.as_view(), name="list_of_trips_selected_between_dates"),
 ]
